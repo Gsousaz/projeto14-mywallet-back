e1eaebbf42ed5e3610970a161d4856a6b42d4441
Gsousaz <doomlighthighlord@gmail.com>
2023-07-06 00:31:27 -0300

    adicionada rota / login

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { MongoClient, ObjectId } from "mongodb";
 import joi from "joi";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from 'uuid';
 
 const app = express(); // App do servidor
 const PORT = 5000;
@@ -28,10 +29,15 @@ const schemaCadastro = joi.object({
     email: joi.string().required().email()
 })
 
+const schemaLogin = joi.object({
+    password: joi.string().required(),
+    email: joi.string().required().email()
+})
+
 
   //-----------------------INICIO DA ROTA POST "/CADASTRO"-----------------------//
 
-  app.post('/sign-up', async (req, res) => {
+  app.post('/cadastro', async (req, res) => {
     try{
 
         const {user, password, email} = req.body;
@@ -46,7 +52,7 @@ const schemaCadastro = joi.object({
         const passwordHash = bcrypt.hashSync(password, 10);
         console.log(passwordHash)
 
-        if (userAlreadyExists) if (userAlreadyExists) return res.sendStatus(409);
+        if (userAlreadyExists) return res.sendStatus(409);
 
         await db.collection('users').insertOne({user, passwordHash, email});
         res.sendStatus(201);
@@ -56,6 +62,38 @@ const schemaCadastro = joi.object({
     }
 
   })
+
+  //-----------------------INICIO DA ROTA POST "/LOGIN"-----------------------//
+
+  app.post("/", async (req, res) => {
+
+    try { 
+
+      const {email, password} = req.body;
+
+      const user = await db.collection('users').findOne({email})
+      if (!user) return res.status(404).send('Email não encontrado')
+
+      const validation = schemaLogin.validate(req.body, {abortEarly: false});
+        
+        if (validation.error) {
+            const errors = validation.error.details.map ((detail) => detail.message);
+            return res.status(422).send(errors);
+        }
+
+      if(user && bcrypt.compareSync(password, user.passwordHash)) {
+        const token = uuid();
+        await db.collection('sessions').insertOne({userId: user._id, token});
+        res.status(200).send(token);
+        
+      } else {
+        res.status(401).send("Senha incorreta")
+      }
+    } catch (err){
+      res.status(500).send(err.message)
+    }
+  })
+
 
 
 app.listen(PORT, () => console.log(`O servidor está rodando na porta ${PORT}`));
